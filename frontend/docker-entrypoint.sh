73c2907b367e7daf7a11ad55b8bedc8cd70e16dd
CreedsCode <contact@creedscode.cc>
2025-06-01 05:17:38 +0200

    fix cube url in docker-compose

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -6,6 +6,7 @@ for file in /usr/share/nginx/html/assets/*.js; do
   sed -i "s|import.meta.env.VITE_CUBE_API_URL|\"${VITE_CUBE_API_URL}\"|g" $file
   sed -i "s|import.meta.env.VITE_API_BASE_URL|\"${VITE_API_BASE_URL}\"|g" $file
   sed -i "s|import.meta.env.VITE_APP_TITLE|\"${VITE_APP_TITLE}\"|g" $file
+  sed -i "s|import.meta.env.VITE_CUBE_API_TOKEN|\"${VITE_CUBE_API_TOKEN}\"|g" $file
 done
 
 # Execute the main container command
